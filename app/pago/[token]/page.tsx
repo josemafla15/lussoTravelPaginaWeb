cf4637417804e43400b8fbcbf2df3274c0c9334a
josemafla15 <josemafla441@gmail.com>
2026-07-08 17:20:19 -0500

    cambios 2

---
+++ app/pago/[token]/page.tsx
@@ -0,0 +1,7 @@
+export default function PagoPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-lusso-charcoal px-6">
+      <p className="text-lusso-cream/70">Esta página no está disponible.</p>
+    </main>
+  );
+}
